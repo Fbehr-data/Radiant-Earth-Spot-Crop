32555f0ac15b3f39160aa453d1dcd71e31085180
Max Langer <max.langer.net@gmail.com>
2022-07-07 14:44:56 +0200

    Changed the qutation marks.

--- src/train_test_function.py
+++ src/train_test_function.py
@@ -15,9 +15,9 @@ def train_test_split_fields(df:pd.DataFrame, train_size=0.7, random_state=42) ->
     # Set a random seed
     np.random.seed(random_state)
     # Split the df into train and test using the field_ids
-    n_fields = df['field_id'].nunique()
-    train_fields = np.random.choice(df['field_id'].unique(), int(n_fields * train_size), replace=False)
-    test_fields = df['field_id'].unique()[~np.in1d(df['field_id'].unique(), train_fields)]
-    df_train = df[df['field_id'].isin(train_fields)]
-    df_test = df[df['field_id'].isin(test_fields)]
+    n_fields = df["field_id"].nunique()
+    train_fields = np.random.choice(df["field_id"].unique(), int(n_fields * train_size), replace=False)
+    test_fields = df["field_id"].unique()[~np.in1d(df["field_id"].unique(), train_fields)]
+    df_train = df[df["field_id"].isin(train_fields)]
+    df_test = df[df["field_id"].isin(test_fields)]
     return df_train, df_test
