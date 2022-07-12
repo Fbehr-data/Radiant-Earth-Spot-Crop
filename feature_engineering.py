836bb5bfb5f5670044c6758f82e0be295f4dc562
Max Langer <max.langer.net@gmail.com>
2022-07-12 13:53:01 +0200

    Added the field size to preprocessing script 3 and the resampling scrip to the feature engineering.

--- feature_engineering.py
+++ feature_engineering.py
@@ -10,6 +10,7 @@ import typer
 import pandas as pd
 from src.find_repo_root import get_repo_root
 from src.feature_engineering_class import FeatureEngineering
+from src.resampling_crop_type import ResamplingProcess
 
 # set root directory
 ROOT_DIR = get_repo_root()
@@ -35,6 +36,11 @@ def calmean():
     feature_engineering = FeatureEngineering(ROOT_DIR=ROOT_DIR, df=df, drop_unknown=True)
     feature_engineering.calculate_mean_temporal()
 
+@engineering.command()
+def resampling():
+    resampling = ResamplingProcess(ROOT_DIR=ROOT_DIR)
+    resampling.start_resampling()
+
 
 if __name__=="__main__":
     print(
@@ -42,6 +48,7 @@ if __name__=="__main__":
         Commands to use: \n \
         1. dropcloud    -   Get started by 'download' the data. \n \
         2. calindices   -   Then 'convert' the images to NPZ files for each . \n \
-        3. calmean      -   Finally 'calculate' the mean of each band for each field for each date. \n"
+        3. calmean      -   Finally 'calculate' the mean of each band for each field for each date. \n \
+        4. resampling   -   Due to the data imbalance a 'resampling' is needed."
         )
     engineering()
