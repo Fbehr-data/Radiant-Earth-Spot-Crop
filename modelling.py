00069fa966bf811eb968601395abb6c733279a52
Max Langer <max.langer.net@gmail.com>
2022-07-12 10:41:09 +0200

    Added the field_size to the preprocessing script.

--- modelling.py
+++ modelling.py
@@ -4,6 +4,7 @@
 # import the needed modules
 # Typer is used as CLI
 import typer
+import xgboost
 from src.find_repo_root import get_repo_root
 
 # set root directory
@@ -12,25 +13,19 @@ ROOT_DIR = get_repo_root()
 # create a Typer object for the preprocessing
 modelling = typer.Typer()
 
-def model_names():
-    return ["XGBoost", "Random Forest", "K-Nearest Neighbors", "Neural Network"]
-
 @modelling.command()
-def select_model(
-    model_name:str=typer.Option(
-        "XGBoost", help="The model to use on the data:", autocompletion=model_names
-    )
-):
-    typer.echo(f"You chose {model_name} as model.")
-
+def choose_model(model_name:str):
+    model_names = {"xgboost": "xgboost script"}
+    print(model_names[model_name]) 
 
 
 if __name__=="__main__":
     print(
         "\n \
         Commands to use: \n \
-        1. download     -   Get started by 'download' the data. \n \
-        2. convert      -   Then 'convert' the images to NPZ files for each . \n \
-        3. calculate    -   Finally 'calculate' the mean of each band for each field for each date. \n"
+        1. train    -   Train/fit the model to the training data. \n \
+        2. optimize -   Use Bayesian optimization to find the best hyperparameters. \n \
+        3. predict  -   Then 'convert' the images to NPZ files for each . \n \
+        4. results  -   Finally 'calculate' the mean of each band for each field for each date. \n"
         )
     modelling()
