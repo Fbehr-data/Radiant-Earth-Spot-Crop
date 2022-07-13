dbffd090583d368408e6a5a49a836ff8f83325d6
Max Langer <max.langer.net@gmail.com>
2022-07-13 23:18:58 +0200

    Formated the src folder with the black formatter.

--- src/eda_functions.py
+++ src/eda_functions.py
@@ -1,19 +1,19 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn.metrics import confusion_matrix
 import seaborn as sns
-import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
 
 # set the plot style
 sns.set_theme(context="notebook", style="darkgrid", palette="crest", font="helvetica")
 cmap = sns.color_palette("crest")
-sns.set(rc = {"figure.dpi":300})
-sns.set(rc = {"figure.figsize":(6,3)})
-sns.set(font_scale = 0.5)
+sns.set(rc={"figure.dpi": 300})
+sns.set(rc={"figure.figsize": (6, 3)})
+sns.set(font_scale=0.5)
 
 
-def get_cloudmask_frame(df:pd.DataFrame, column:str) -> pd.DataFrame:
-    """ Create a data frame which is easy to plot with the number of observations 
+def get_cloudmask_frame(df: pd.DataFrame, column: str) -> pd.DataFrame:
+    """Create a data frame which is easy to plot with the number of observations
         for the unclouded or no information state for a given column of the data frame.
 
     Args:
@@ -23,11 +23,9 @@ def get_cloudmask_frame(df:pd.DataFrame, column:str) -> pd.DataFrame:
     Returns:
         pd.DataFrame: Data frame that is ready to plot.
     """
-    # create two series with the number of observations that are unclouded or have no CLM information 
-    unclouded = df[df["CLM"]==0].groupby(column)[column] \
-    .count()
-    noinfo = df[df["CLM"]==255].groupby(column)[column] \
-    .count()
+    # create two series with the number of observations that are unclouded or have no CLM information
+    unclouded = df[df["CLM"] == 0].groupby(column)[column].count()
+    noinfo = df[df["CLM"] == 255].groupby(column)[column].count()
     # create a data frame of the two series
     cloudmask_df = pd.concat([unclouded, noinfo], axis=1)
     cloudmask_df.columns = ["unclouded", "no information"]
@@ -39,22 +37,22 @@ def get_cloudmask_frame(df:pd.DataFrame, column:str) -> pd.DataFrame:
     return cloudmask_df
 
 
-def get_count_per_field_id(df:pd.DataFrame, column:str) -> pd.DataFrame:
-    """ Create a data frame with the amount of observations 
+def get_count_per_field_id(df: pd.DataFrame, column: str) -> pd.DataFrame:
+    """Create a data frame with the amount of observations
         for the given column per field_id.
 
     Args:
-        df (pd.DataFrame):  Data frame containing the field_id and 
+        df (pd.DataFrame):  Data frame containing the field_id and
                             at least another column to count the observations.
-        column (str):       The column to count the observations for. 
+        column (str):       The column to count the observations for.
 
     Returns:
         pd.DataFrame: Data frame with the count per field_id.
     """
-    # create a list of series with the occurrence of field_ids per month 
+    # create a list of series with the occurrence of field_ids per month
     counts = []
     for entry in sorted(df[column].unique()):
-        count_perID = df[df[column]==entry].groupby("field_id")["field_id"].count()
+        count_perID = df[df[column] == entry].groupby("field_id")["field_id"].count()
         counts.append(count_perID)
     # create a data frame of list of series
     perID_df = pd.concat(counts, axis=1)
@@ -65,8 +63,8 @@ def get_count_per_field_id(df:pd.DataFrame, column:str) -> pd.DataFrame:
     return perID_df
 
 
-def plot_confusion_matrix(xgb_cm:confusion_matrix, labels:list):
-    """ Plots a nicely formatted confusion matrix. 
+def plot_confusion_matrix(xgb_cm: confusion_matrix, labels: list):
+    """Plots a nicely formatted confusion matrix.
 
     Args:
         xgb_cm (sklearn.metrics.confusion_matrix): The confusion matrix to be plotted.
@@ -75,22 +73,22 @@ def plot_confusion_matrix(xgb_cm:confusion_matrix, labels:list):
 
     # setup the heatmap of the confusion matrix
     ax = sns.heatmap(xgb_cm, annot=True, cmap=cmap, fmt="g")
-    ax.set_title("Confusion Matrix with labels\n");
+    ax.set_title("Confusion Matrix with labels\n")
     ax.set_xlabel("\nPredicted Values")
-    ax.set_ylabel("Actual Values ");
+    ax.set_ylabel("Actual Values ")
 
     # set the ticket labels
     ax.xaxis.set_ticklabels(labels)
     ax.yaxis.set_ticklabels(labels)
-    plt.yticks(rotation=0) 
-    plt.xticks(rotation=90, horizontalalignment="center") 
+    plt.yticks(rotation=0)
+    plt.xticks(rotation=90, horizontalalignment="center")
 
     # display the confusion matrix
     plt.show()
 
 
-def get_label_accuracies(xgb_cm:confusion_matrix, labels:list) -> pd.DataFrame:
-    """ Gives back a data frame with the accuracy for each class label 
+def get_label_accuracies(xgb_cm: confusion_matrix, labels: list) -> pd.DataFrame:
+    """Gives back a data frame with the accuracy for each class label
         and the class it is most often confused with, with a percentage of how often.
 
     Args:
@@ -106,28 +104,28 @@ def get_label_accuracies(xgb_cm:confusion_matrix, labels:list) -> pd.DataFrame:
     confusion_percent = []
 
     for idx, label in enumerate(labels):
-      # calculate the accuracy for the current label
-      highest_value = np.diagonal(xgb_cm)[idx]
-      sum_of_labels = np.sum(xgb_cm[idx])
-      label_accuracy = round(highest_value/sum_of_labels, 2)
-      accuracies.append(label_accuracy)
+        # calculate the accuracy for the current label
+        highest_value = np.diagonal(xgb_cm)[idx]
+        sum_of_labels = np.sum(xgb_cm[idx])
+        label_accuracy = round(highest_value / sum_of_labels, 2)
+        accuracies.append(label_accuracy)
 
-      # identify the label that the current one is most often confused with
-      second_highest_value = np.partition(xgb_cm[idx].flatten(), -2)[-2]
-      second_highest_value_idx = (np.where(xgb_cm[idx] == second_highest_value))
-      often_confused_with.append(labels[int(second_highest_value_idx[0])])
+        # identify the label that the current one is most often confused with
+        second_highest_value = np.partition(xgb_cm[idx].flatten(), -2)[-2]
+        second_highest_value_idx = np.where(xgb_cm[idx] == second_highest_value)
+        often_confused_with.append(labels[int(second_highest_value_idx[0])])
 
-      # get the confusion in percentage
-      confusion = round(second_highest_value/highest_value*100, 2)
-      confusion_percent.append(confusion)
+        # get the confusion in percentage
+        confusion = round(second_highest_value / highest_value * 100, 2)
+        confusion_percent.append(confusion)
 
     # create a data frame using a dictionary
     data = {
-        "crop_type":labels, 
-        "accuracy":accuracies,
-        "often_confused_with":often_confused_with,
-        "confusion_percent":confusion_percent
-        }
+        "crop_type": labels,
+        "accuracy": accuracies,
+        "often_confused_with": often_confused_with,
+        "confusion_percent": confusion_percent,
+    }
     acc_df = pd.DataFrame(data)
 
     # sort the data frame in order decreasing feature importance
@@ -136,8 +134,8 @@ def get_label_accuracies(xgb_cm:confusion_matrix, labels:list) -> pd.DataFrame:
     return acc_df
 
 
-def plot_label_accuracy(df:pd.DataFrame):
-    """ Plot the accuracy per label as a bar chart. 
+def plot_label_accuracy(df: pd.DataFrame):
+    """Plot the accuracy per label as a bar chart.
 
     Args:
         df (pd.DataFrame): Data frame generated by the get_label_accuracies function.
@@ -147,11 +145,14 @@ def plot_label_accuracy(df:pd.DataFrame):
 
     # do the annotation on each bar
     for bar in ax.patches:
-        ax.annotate(format(bar.get_height(), ".2f"), 
-                      (bar.get_x() + bar.get_width() / 2., bar.get_height()), 
-                      ha = "center", va = "center", 
-                      xytext = (0, 9), 
-                      textcoords = "offset points")
+        ax.annotate(
+            format(bar.get_height(), ".2f"),
+            (bar.get_x() + bar.get_width() / 2.0, bar.get_height()),
+            ha="center",
+            va="center",
+            xytext=(0, 9),
+            textcoords="offset points",
+        )
 
     # set chart labels
     ax.set_title("Accuracy of the different crop types\n")
@@ -159,7 +160,7 @@ def plot_label_accuracy(df:pd.DataFrame):
     ax.set_ylabel("Accuracy")
 
     # set the ticket labels
-    plt.yticks(rotation=0) 
+    plt.yticks(rotation=0)
     plt.ylim(0, 1.1)
     plt.xticks(rotation=90, horizontalalignment="center")
 
@@ -167,7 +168,9 @@ def plot_label_accuracy(df:pd.DataFrame):
     plt.show()
 
 
-def plot_feature_importance(importance:list, feature_names:list, model_name:str, num_features=None):
+def plot_feature_importance(
+    importance: list, feature_names: list, model_name: str, num_features=None
+):
     """_summary_
 
     Args:
@@ -182,17 +185,17 @@ def plot_feature_importance(importance:list, feature_names:list, model_name:str,
     feature_names = np.array(feature_names)
 
     # create a data frame using a dictionary
-    data={"feature_names":feature_names, "feature_importance":feature_importance}
+    data = {"feature_names": feature_names, "feature_importance": feature_importance}
     fi_df = pd.DataFrame(data)
 
     # sort the data frame in order decreasing feature importance
     fi_df.sort_values(by=["feature_importance"], ascending=False, inplace=True)
-    
+
     # set the number of features to display
     fi_df = fi_df.head(num_features)
-    
+
     # define size of bar plot
-    plt.figure(figsize=(10,8))
+    plt.figure(figsize=(10, 8))
 
     # plot Searborn bar chart
     sns.barplot(x=fi_df["feature_importance"], y=fi_df["feature_names"], color=cmap[4])
--- src/knn_model.py
+++ src/knn_model.py
@@ -2,58 +2,58 @@
 # depending on colab or vscode environment
 on_colabs = False
 import sys
+
 if on_colabs:
-  from google.colab import drive
-  drive.mount("/content/drive")
-  OUTPUT_DIR = "/content/drive/MyDrive/Radiant_Earth_Spot_Crop/data"
-  sys.path.append("/content/drive/MyDrive/Radiant_Earth_Spot_Crop/src")
+    from google.colab import drive
+
+    drive.mount("/content/drive")
+    OUTPUT_DIR = "/content/drive/MyDrive/Radiant_Earth_Spot_Crop/data"
+    sys.path.append("/content/drive/MyDrive/Radiant_Earth_Spot_Crop/src")
 else:
-  OUTPUT_DIR = "./data"
-  sys.path.append("./src/")
+    OUTPUT_DIR = "./data"
+    sys.path.append("./src/")
+
+# ignore warnings
+import warnings
 
 # import the needed modules
 import numpy as np
 import pandas as pd
 
 # import the machine learning modules
-# The goal of this file will be the modeling based on the KNN 
+# The goal of this file will be the modeling based on the KNN
 from imblearn.over_sampling import SMOTE
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import (
     accuracy_score,
+    classification_report,
+    confusion_matrix,
     f1_score,
     log_loss,
-    confusion_matrix,
-    classification_report
-    )
+)
+from sklearn.model_selection import GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
 
-# ignore warnings
-import warnings
 warnings.filterwarnings("ignore")
+
+import matplotlib.pyplot as plt
 
 # import plotting modules and set the style for the figures
 import seaborn as sns
-import matplotlib.pyplot as plt
+
 #%matplotlib inline
-sns.set_theme(
-    context="notebook",
-    style="darkgrid",
-    palette="crest",
-    font="helvetica"
-    )
-cmap = sns.color_palette("crest", 6)    # six colors are created this way
-sns.set(rc = {"figure.dpi":300})        # size of the figues and font size are created this way
-sns.set(rc = {"figure.figsize":(6,3)})
-sns.set(font_scale = 0.5)
+sns.set_theme(context="notebook", style="darkgrid", palette="crest", font="helvetica")
+cmap = sns.color_palette("crest", 6)  # six colors are created this way
+sns.set(rc={"figure.dpi": 300})  # size of the figues and font size are created this way
+sns.set(rc={"figure.figsize": (6, 3)})
+sns.set(font_scale=0.5)
 
 # set a random seed
 RSEED = 42
 np.random.seed(RSEED)
 
 # load the base data from the CSV files, including Train and Test dataset
-dataset_name = 'Train_Dataset4'
-dataset_test ='Test_Dataset.csv'
+dataset_name = "Train_Dataset4"
+dataset_test = "Test_Dataset.csv"
 df_train = pd.read_csv(f"{OUTPUT_DIR}/{dataset_name}.csv")
 df_test = pd.read_csv(f"{OUTPUT_DIR}/{dataset_test}.csv")
 
@@ -68,11 +68,20 @@ y_val = df_test["label"]
 y_val = y_val.astype(int)
 
 # set the class labels from 0 to 8
-y_train = y_train-1
-y_val = y_val-1
+y_train = y_train - 1
+y_val = y_val - 1
 
 # Fitting the KN model
-kn = KNeighborsClassifier(n_neighbors=5, weights='uniform', algorithm='auto', leaf_size=30, p=2, metric='minkowski', metric_params=None, n_jobs=None)
+kn = KNeighborsClassifier(
+    n_neighbors=5,
+    weights="uniform",
+    algorithm="auto",
+    leaf_size=30,
+    p=2,
+    metric="minkowski",
+    metric_params=None,
+    n_jobs=None,
+)
 kn.fit(X_train, y_train)
 
 # predict the absolute classes and probabilities
@@ -83,13 +92,17 @@ y_pred_val = kn.predict(X_val)
 y_proba_train = kn.predict_proba(X_train)
 y_proba_val = kn.predict_proba(X_val)
 
-# print the results for accuracy, F1-score and cross entropy 
+# print the results for accuracy, F1-score and cross entropy
 print("---" * 12)
 print(f"Accuracy on train data: {round(accuracy_score(y_train, y_pred_train), 3)}")
 print(f"Accuracy on test data: {round(accuracy_score(y_val, y_pred_val), 3)}")
 print("---" * 12)
-print(f'F1-score on train data: {round(f1_score(y_train, y_pred_train, average="macro"), 3)}')
-print(f'F1-score on test data: {round(f1_score(y_val, y_pred_val, average="macro"), 3)}')
+print(
+    f'F1-score on train data: {round(f1_score(y_train, y_pred_train, average="macro"), 3)}'
+)
+print(
+    f'F1-score on test data: {round(f1_score(y_val, y_pred_val, average="macro"), 3)}'
+)
 print("---" * 12)
 print(f"Cross-entropy on train data: {round(log_loss(y_train, y_proba_train), 3)}")
 print(f"Cross-entropy on test data: {round(log_loss(y_val, y_proba_val), 3)}")
